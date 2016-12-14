a8a974d4799ac6cfabf83a77c8e1bcc2b6d8828c
Radu Matei <matei.radu94@gmail.com>
2016-12-14 15:15:05 +0200

    Added temperature telemetry class

---
+++ windows10-iot/session03/CloudTelemetry/CloudTelemetry.Common/TemperatureTelemetry.cs
@@ -0,0 +1,9 @@
+﻿namespace CloudTelemetry.Common
+{
+    public class TemperatureTelemetry
+    {
+        public string Time { get; set; }
+
+        public double Temperature { get; set; }
+    }
+}
