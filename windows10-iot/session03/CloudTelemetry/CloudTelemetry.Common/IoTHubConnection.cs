ac6f1c7d2c5abc5c2fe8de38fae074804b882e52
Radu Matei <matei.radu94@gmail.com>
2016-12-14 15:15:12 +0200

    Added IoT Hub connection class

---
+++ windows10-iot/session03/CloudTelemetry/CloudTelemetry.Common/IoTHubConnection.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Azure.Devices.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudTelemetry.Common
+{
+    public class IoTHubConnection : IDisposable
+    {
+        private DeviceClient _deviceClient { get; set; }
+
+        public IoTHubConnection()
+        {
+            _deviceClient = DeviceClient.CreateFromConnectionString(GetConnectionString(), TransportType.Amqp);
+        }
+
+        public async Task SendEventAsync(string payload)
+        {
+            await _deviceClient.SendEventAsync(new Message(Encoding.ASCII.GetBytes(payload)));
+        }
+
+        private string GetConnectionString()
+        {
+            return "your-device-connection-string";
+        }
+
+        public void Dispose()
+        {
+            _deviceClient.Dispose();
+        }
+    }
+}
