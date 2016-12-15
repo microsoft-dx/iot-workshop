1f903a2c61ae6c6242b4da8708a72480d8ae4b4c
Radu Matei <matei.radu94@gmail.com>
2016-12-15 21:09:40 +0200

    Added Sense Hat client

---
+++ windows10-iot/session03/CloudTelemetry/CloudTelemetry.SenseHat/SenseHat.cs
@@ -0,0 +1,45 @@
+﻿using CloudTelemetry.Common;
+using Emmellsoft.IoT.Rpi.SenseHat;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CloudTelemetry.SenseHat
+{
+    public class SenseHat : IDisposable
+    {
+        private ISenseHat _senseHat { get; set; }
+
+        public async Task Activate()
+        {
+            _senseHat = await SenseHatFactory.GetSenseHat().ConfigureAwait(false);
+
+            _senseHat.Display.Clear();
+            _senseHat.Display.Update();
+        }
+
+        public TemperatureTelemetry GetTemperature()
+        {
+            while (true)
+            {
+                _senseHat.Sensors.HumiditySensor.Update();
+
+                if (_senseHat.Sensors.Temperature.HasValue)
+                {
+                    return new TemperatureTelemetry()
+                    {
+                        Time = DateTime.UtcNow.AddHours(3).ToString("yyyy-MM-dd HH:mm:ss.fff"),
+                        Temperature = Math.Round(_senseHat.Sensors.Temperature.Value, 2)
+                    };
+                }
+
+                else new ManualResetEventSlim(false).Wait(TimeSpan.FromSeconds(0.5));
+            }
+        }
+
+        public void Dispose()
+        {
+            _senseHat.Dispose();
+        }
+    }
+}
