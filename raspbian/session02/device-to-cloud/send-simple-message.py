8405f385c33f13d9b70c1869e4d23bdec9d4cbbc
Radu Matei <matei.radu94@gmail.com>
2016-12-17 02:19:06 +0200

    Added code to send data to cloud

--- raspbian/session02/device-to-cloud/send-simple-message.py
+++ raspbian/session02/device-to-cloud/send-simple-message.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from deviceClient import DeviceClient
+
 
 KEY = "device-primary-key";
 HUB = "iot-hub-name";
@@ -8,4 +10,7 @@ DEVICE_NAME = "device-name";
 device = DeviceClient(HUB, DEVICE_NAME, KEY)
 device.create_sas(600)
 
-print(device.send(b"{message: 'This is awesome! Hello from Python!'}"))
+time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+message = "Hello at {0}, this is awesome!".format(time)
+
+print(device.send(str.encode(message)))
